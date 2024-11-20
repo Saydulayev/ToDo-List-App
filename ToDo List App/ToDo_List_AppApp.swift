84076f8e7a05945d5b62dd13b368e0e281118af8
Saydulayev <saydulayev.wien@gmail.com>
2024-11-20 08:59:30 +0100

    Initial Commit

---
+++ ToDo List App/ToDo_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_List_AppApp.swift
+//  ToDo List App
+//
+//  Created by Saydulayev on 20.11.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
